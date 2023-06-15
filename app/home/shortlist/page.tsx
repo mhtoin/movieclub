d3e4a05bd35c129b8fc02abfbf80c47ada560233
Miika Oinonen <miika.oinonen@maplet.com>
2023-06-15 21:03:48 +0300

    fixed non-unique keys in all shortlists

--- app/home/shortlist/page.tsx
+++ app/home/shortlist/page.tsx
@@ -27,8 +27,8 @@ export default async function ShortList() {
         {allShortlists.map(shortlist => {
           return (
             <>
-            <h1 className="text-xl m-5" key={shortlist.id}>{shortlist.user.name}</h1>
-            <div className="flex flex-row gap-5 w-2/3 sm:w-auto">
+            <h1 key={shortlist.id + '-title'} className="text-xl m-5" key={shortlist.id}>{shortlist.user.name}</h1>
+            <div key={shortlist.id + '-container'} className="flex flex-row gap-5 w-2/3 sm:w-auto">
               {shortlist.movies.map(movie => {
                 return <ShortListItem key={shortlist.id + movie.id} movie={movie} shortlistId={shortlist.id} />
               })}
