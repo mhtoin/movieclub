6036df10b216b3dcbf0e1a6e8cfc2f3dfe37e410
Miika Oinonen <mhtoin@utu.fi>
2023-07-27 14:32:11 +0300

    forcing dark mode because it looks objectively better

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className="min-h-screen bg-black">
       <Notification />
          {/* @ts-expect-error Server Component */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -22,6 +23,7 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
+    themes: ["dark"],
     base: false
   }
 }
