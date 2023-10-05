67e5840d5951d16eb67870d186e564e8554f8e63
Miika Oinonen <mhtoin@gmail.com>
2023-10-05 11:39:39 +0300

    Add loading layout to search page

--- app/home/shortlist/edit/search/layout.tsx
+++ app/home/shortlist/edit/search/layout.tsx
@@ -12,10 +12,7 @@ export default function SearchLayout({
     <div className="flex flex-col items-center gap-5 z-10">
       <ShortlistContainer />
       <Filters />
-
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 z-30">
-        {children}
-      </div>
+      {children}
     </div>
   );
 }
--- app/home/shortlist/edit/search/page.tsx
+++ app/home/shortlist/edit/search/page.tsx
@@ -86,7 +86,7 @@ export default function SearchPage() {
     : [];
 
   return (
-    <>
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 z-30">
         {data
           ? data?.pages?.map((page) => (
               <Fragment key={page.page}>
@@ -108,6 +108,6 @@ export default function SearchPage() {
           Load More
         </button>
       )}
-    </>
+    </div>
   );
 }
