422f778b54ed99fd151f652da609645a6e3eef66
Miika Oinonen <mhtoin@gmail.com>
2023-11-18 14:52:40 +0200

    Fix wrong import for authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,5 @@ import "dotenv/config";
 import NextAuth from "next-auth";
 import { authOptions } from "./auth";
 
-
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
 //import { getServerSession } from "@/lib/deprecated_getServerSession";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-
+import { authOptions } from "./api/auth/[...nextauth]/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   const isAuthenticated = !!session;
 
   if (isAuthenticated) {
-    redirect('/home')
+    redirect("/home");
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
