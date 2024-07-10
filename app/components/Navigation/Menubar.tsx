538ec8381d205ed71c4e634b2d818dd8dade5690
Miika Oinonen <mhtoin@gmail.com>
2024-07-10 22:04:29 +0300

    Menu components and styling

--- app/components/Navigation/Menubar.tsx
+++ app/components/Navigation/Menubar.tsx
@@ -1,7 +1,10 @@
 import * as Ariakit from "@ariakit/react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Menubar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <Ariakit.MenuProvider>
       <Ariakit.MenuButton>
@@ -16,23 +19,39 @@ export default function Menubar() {
         gutter={10}
         className="relative gap-2 flex z-50 max-h-76 min-w-[180px] flex-col overflow-auto overscroll-contain rounded border bg-popoverbg p-5 border-white/15"
       >
-        <Ariakit.MenuItem className="flex cursor-default scroll-m-1 items-center gap-1 rounded p-1 data-[active-item]:bg-primary active:bg-primary data-[data-active]:bg:primary data-[data-active]:py-2">
+        <Ariakit.MenuItem className="menu-item">
           <Link href={"/dashboard"}>Dashboard</Link>
+          <span className="text-xs text-white/50">View stats</span>
         </Ariakit.MenuItem>
-        <Ariakit.MenuItem className="flex cursor-default scroll-m-1 items-center gap-1 rounded p-1 data-[active-item]:bg-primary active:bg-primary data-[data-active]:bg:primary data-[data-active]:py-2">
+        <Ariakit.MenuItem className="menu-item">
           <Link href={"/home/shortlist"}>Shortlist</Link>
+          <span className="text-xs text-white/50">
+            View all shortlists from users
+          </span>
         </Ariakit.MenuItem>
-        <Ariakit.MenuItem className="flex cursor-default ml-10 scroll-m-1 items-center gap-1 rounded p-1 data-[active-item]:bg-primary active:bg-primary data-[data-active]:bg:primary data-[data-active]:py-2">
-          <Link href={"/home/shortlist"}>Edit</Link>
+        <Ariakit.MenuItem className="menu-item menu-item-inner">
+          <div className="column">
+            <Link href={"/home/shortlist"}>Search</Link>
+            <span className="text-xs text-white/50">Search for movies</span>
+          </div>
         </Ariakit.MenuItem>
-        <Ariakit.MenuItem className="flex cursor-default ml-10 scroll-m-1 items-center gap-1 rounded p-1 data-[active-item]:bg-primary active:bg-primary data-[data-active]:bg:primary data-[data-active]:py-2">
-          <Link href={"/home/shortlist"}>Watchlist</Link>
+        <Ariakit.MenuItem className="menu-item menu-item-inner">
+          <div className="column">
+            <Link href={"/home/shortlist"}>Watchlist</Link>
+            <span className="text-xs text-white/50">View your watchlist</span>
+          </div>
         </Ariakit.MenuItem>
-        <Ariakit.MenuItem className="flex cursor-default scroll-m-1 items-center gap-1 rounded p-1 data-[active-item]:bg-primary active:bg-primary data-[data-active]:bg:primary data-[data-active]:py-2">
+        <Ariakit.MenuItem className="menu-item">
           <Link href={"/tierlists"}>Tierlists</Link>
+          <span className="text-xs text-white/50">
+            View all tierlists from users
+          </span>
         </Ariakit.MenuItem>
-        <Ariakit.MenuItem className="flex cursor-default ml-10 scroll-m-1 items-center gap-1 rounded p-1 data-[active-item]:bg-primary active:bg-primary data-[data-active]:bg:primary data-[data-active]:py-2">
-          <Link href={"/home/shortlist"}>Edit</Link>
+        <Ariakit.MenuItem className="menu-item menu-item-inner">
+          <div className="column">
+            <Link href={"/home/shortlist"}>Edit</Link>
+            <span className="text-xs text-white/50">Edit your tierlist</span>
+          </div>
         </Ariakit.MenuItem>
       </Ariakit.Menu>
     </Ariakit.MenuProvider>
