006352ef10094be069c6d64b74db8882ac6bae0b
Miika Oinonen <mhtoin@utu.fi>
2025-02-16 16:47:43 +0200

    Test fixing path issues in importing

--- next.config.js
+++ next.config.js
@@ -1,29 +1,38 @@
+const path = require("node:path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  experimental: {},
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-        port: "",
-        pathname: "/t/p/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.discordapp.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/u/**",
-      },
-    ],
-  },
+	output: "standalone",
+	experimental: {},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "image.tmdb.org",
+				port: "",
+				pathname: "/t/p/**",
+			},
+			{
+				protocol: "https",
+				hostname: "cdn.discordapp.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+				port: "",
+				pathname: "/u/**",
+			},
+		],
+	},
+	webpack: (config) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			components: path.join(__dirname, "components"),
+		};
+		return config;
+	},
 };
 
 module.exports = nextConfig;
