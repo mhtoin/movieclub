2670209ebf9ec9a8126a3b99b16892ad39dd815c
Miika Oinonen <mhtoin@utu.fi>
2025-03-05 21:01:00 +0200

    Fix wrong id in recommended card

--- components/search/RecommendedCard.tsx
+++ components/search/RecommendedCard.tsx
@@ -124,7 +124,7 @@ export default function RecommendedCard({
 					<div className="description-links">
 						<div className="flex flex-row items-center gap-2">
 							<Link
-								href={`https://www.themoviedb.org/movie/${movie.id}`}
+								href={`https://www.themoviedb.org/movie/${movie.tmdbId}`}
 								target="_blank"
 							>
 								<Button variant="ghost" size="icon">
