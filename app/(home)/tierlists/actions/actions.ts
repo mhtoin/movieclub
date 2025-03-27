17e72b5da4f0912a543c5bd7517ec683dfecf042
Miika Oinonen <mhtoin@utu.fi>
2025-03-27 21:09:43 +0200

    Remove unused packages and imports

--- app/(home)/tierlists/actions/actions.ts
+++ app/(home)/tierlists/actions/actions.ts
@@ -1,24 +1,9 @@
 "use server";
-
-import { validateRequest } from "@/lib/auth";
-import { createTierlist, modifyTierlist } from "@/lib/tierlists";
-import type { Tierlists } from "@prisma/client";
+import { createTierlist } from "@/lib/tierlists";
 import { redirect } from "next/navigation";
 
 export async function createNewTierlist(formData: FormData) {
 	await createTierlist(formData);
 	redirect("/tierlists/");
 	//return createTierlist
-}
-
-export async function saveTierlist(tierlist: Tierlists) {
-	return tierlist;
-}
-
-export async function recreateTierlist(formData: FormData) {
-	const { user } = await validateRequest();
-
-	const userId = user?.id;
-	await modifyTierlist(formData);
-	redirect(`/tierlists/${userId}`);
 }
