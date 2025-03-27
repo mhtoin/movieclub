78f6ce8035da2bd1bc2c39831f375b416a186c85
Miika Oinonen <mhtoin@utu.fi>
2025-03-27 20:38:27 +0200

    Remove unused packages, misc fixes

--- scripts/watchdates.ts
+++ scripts/watchdates.ts
@@ -2,31 +2,31 @@ import { formatISO } from "date-fns";
 import prisma from "../lib/prisma";
 
 async function migrateDates() {
-  const movies = await prisma.movie.findMany({
-    where: {
-      movieOfTheWeek: {
-        not: null,
-      },
-    },
-  });
+	const movies = await prisma.movie.findMany({
+		where: {
+			movieOfTheWeek: {
+				not: null,
+			},
+		},
+	});
 
-  movies.forEach(async (movie) => {
-    const movieOfTheWeek = movie.movieOfTheWeek;
+	for (const movie of movies) {
+		const movieOfTheWeek = movie.movieOfTheWeek;
 
-    if (!movieOfTheWeek) {
-      return;
-    }
-    const formatted = formatISO(movieOfTheWeek, { representation: "date" });
+		if (!movieOfTheWeek) {
+			return;
+		}
+		const formatted = formatISO(movieOfTheWeek, { representation: "date" });
 
-    await prisma.movie.update({
-      where: {
-        id: movie.id,
-      },
-      data: {
-        watchDate: formatted,
-      },
-    });
-  });
+		await prisma.movie.update({
+			where: {
+				id: movie.id,
+			},
+			data: {
+				watchDate: formatted,
+			},
+		});
+	}
 }
 
 migrateDates();
