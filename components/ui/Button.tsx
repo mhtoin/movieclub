99567d0630a0f701dc4326b584d50231a9fb7951
Miika Oinonen <mhtoin@utu.fi>
2025-03-31 21:51:23 +0300

    Button color consistency, ensure text legibility

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
 				destructive:
 					"bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
 				outline:
-					"border border-input bg-background shadow-sm hover:bg-accent/80 hover:text-accent-foreground",
+					"border border-input bg-background shadow-sm hover:bg-accent/80 hover:border-accent/80 hover:text-accent-foreground",
 				secondary:
 					"bg-secondary text-secondary-foreground shadow-sm hover:bg-accent/80",
 				ghost: "hover:bg-accent/80 hover:text-accent-foreground",
