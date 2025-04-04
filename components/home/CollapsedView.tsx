a85128f5cb16f2d8b0f6fa576b5ab51c568589cd
Miika Oinonen <mhtoin@utu.fi>
2025-04-04 22:28:36 +0300

    Add user to movie posters

--- components/home/CollapsedView.tsx
+++ components/home/CollapsedView.tsx
@@ -6,10 +6,15 @@ export default function CollapsedView({
 	movie: MovieWithReviews;
 }) {
 	return (
-		<div className="flex w-full h-full items-center">
+		<div className="flex flex-col h-full justify-center gap-5 px-2">
 			<h2 className="text-3xl font-bold [writing-mode:vertical-lr] text-primary-foreground">
 				{movie.title.toLocaleUpperCase()}
 			</h2>
+			<img
+				src={movie.user?.image}
+				alt={movie.user?.name}
+				className="w-8 h-8 rounded-full"
+			/>
 		</div>
 	);
 }
