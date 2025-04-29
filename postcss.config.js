869a6d22fe80f502b46f8c82cc0ef53d13f2e1b0
Miika Oinonen <mhtoin@utu.fi>
2025-04-29 14:58:50 +0300

    Revert postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    "postcss-easing-gradients": {},
+  },
 };
-
-export default config;
