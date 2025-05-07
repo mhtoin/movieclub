e9dc614b61cd480d9f2afecab8ff0ca7e1c3d94e
Miika Oinonen <mhtoin@utu.fi>
2025-05-07 15:07:59 +0300

    Move db to postgres and neondb

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,1 +1,78 @@
-declare global {}
+declare global {
+  namespace PrismaJson {
+    type GenresType = Array<{ id: number; name: string }>
+    type WatchProvidersType = Array<{
+      id: number
+      providers: Array<Provider>
+      link: string
+    }>
+
+    type ConfigProviders = Array<Provider>
+
+    type Provider = {
+      logo_path: string
+      provider_name: string
+      provider_id: number
+      display_priority: number
+    }
+
+    type Images = {
+      backdrops: SingleImage[]
+      posters: SingleImage[]
+      logos: SingleImage[]
+    }
+
+    type SingleImage = {
+      aspect_ratio: number
+      file_path: string
+      height: number
+      width: number
+      iso_639_1?: string
+      vote_average: number
+      vote_count: number
+      blurDataUrl?: string
+    }
+
+    type Videos = Array<{
+      iso_639_1: string
+      iso_3166_1: string
+      name: string
+      key: string
+      site: string
+      size: number
+      type: string
+      official: boolean
+      published_at: string
+      id: string
+    }>
+
+    type CrewMembers = Array<{
+      adult: boolean
+      gender: number
+      id: number
+      known_for_department: string
+      name: string
+      original_name: string
+      popularity: number
+      profile_path?: string
+      credit_id: string
+      department: string
+      job: string
+    }>
+
+    type CastMembers = Array<{
+      adult: boolean
+      gender: number
+      id: number
+      known_for_department: string
+      name: string
+      original_name: string
+      popularity: number
+      profile_path?: string
+      cast_id: number
+      character: string
+      credit_id: string
+      order: number
+    }>
+  }
+}
