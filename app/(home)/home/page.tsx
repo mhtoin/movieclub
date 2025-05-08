40966d17cad1fc80817a6bf0fceb9b01545c0d4e
Miika Oinonen <mhtoin@utu.fi>
2025-05-08 20:57:41 +0300

    Remove watch history for now - move into separate system later

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -3,23 +3,13 @@ export const revalidate = 0
 import { CurrentMovieWrapper } from '@/components/home/CurrentMovieWrapper'
 import { getCurrentSession } from '@/lib/authentication/session'
 import { getQueryClient } from '@/lib/getQueryClient'
-import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
-import MoviesOfTheMonth from 'components/home/MoviesOfTheMonth'
+import { dehydrate } from '@tanstack/react-query'
 import { format } from 'date-fns'
 import { getMoviesOfTheWeekByMonth } from 'lib/movies/movies'
 import { redirect } from 'next/navigation'
 import { Suspense } from 'react'
 
 export default async function HomePage() {
-  const queryClient = getQueryClient()
-
-  queryClient.prefetchInfiniteQuery({
-    queryKey: ['pastMovies'],
-    queryFn: () => getMoviesOfTheWeekByMonth(format(new Date(), 'yyyy-MM')),
-    initialPageParam: format(new Date(), 'yyyy-MM'),
-  })
-
-  const dehydratedState = dehydrate(queryClient)
   const { user } = await getCurrentSession()
 
   if (!user) {
@@ -33,11 +23,6 @@ export default async function HomePage() {
           <CurrentMovieWrapper />
         </Suspense>
       </div>
-      <HydrationBoundary state={dehydratedState}>
-        <Suspense fallback={null}>
-          <MoviesOfTheMonth />
-        </Suspense>
-      </HydrationBoundary>
     </div>
   )
 }
