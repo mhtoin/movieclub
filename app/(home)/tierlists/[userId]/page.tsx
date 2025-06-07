82b45325e14008b2a90fd8d1716bd33215edc11e
Miika Oinonen <mhtoin@utu.fi>
2025-06-07 23:06:13 +0300

    Fix tierlist placements on main page

--- app/(home)/tierlists/[userId]/page.tsx
+++ app/(home)/tierlists/[userId]/page.tsx
@@ -13,7 +13,7 @@ export default async function TierlistPage({
     <div className="flex h-screen flex-col items-center justify-center gap-5">
       <h1 className="text-3xl font-bold">Tierlists</h1>
       <CreateDialog />
-      <div className="flex flex-col items-center justify-center w-full max-w-2xl gap-5 overflow-auto">
+      <div className="flex p-5 items-center flex-wrap justify-center w-full max-w-2xl gap-5 overflow-auto ">
         {tierlists.map((tierlist) => (
           <div key={tierlist.id} className="mb-2">
             <TierlistCard tierlist={tierlist} />
