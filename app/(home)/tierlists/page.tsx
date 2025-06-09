91d137a72ac8cb1ec1df5ae924a55f526ea9a1c0
Miika Oinonen <mhtoin@utu.fi>
2025-06-09 17:23:53 +0300

    Leftover additions to tierlist

--- app/(home)/tierlists/page.tsx
+++ app/(home)/tierlists/page.tsx
@@ -1,3 +1,4 @@
+import { getUsers } from "@/lib/user"
 import { Button } from "components/ui/Button"
 import { getCurrentSession } from "lib/authentication/session"
 import { getTierlists } from "lib/tierlists"
@@ -14,37 +15,12 @@ export default async function Tierlists() {
   if (!user) {
     redirect("/")
   }
-  const allTierlists = await getTierlists()
-  console.log(allTierlists)
-  const usersWithTierlist = allTierlists.map((tierlist) => tierlist.userId)
+  const allTierlists = await getUsers()
 
   return (
     <div className="flex flex-col items-center gap-10 pt-20">
       <h1 className="text-2xl font-bold">Tierlists</h1>
-      <div>
-        {!usersWithTierlist.includes(user?.id || "") && (
-          <form action={createNew}>
-            <Button variant="outline" type="submit">
-              Create new
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <title>Create new</title>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2h6Z"
-                />
-              </svg>
-            </Button>
-          </form>
-        )}
-      </div>
+
       <div className="grid grid-cols-2 gap-10">
         {allTierlists.map((tierlist) => {
           return (
@@ -52,11 +28,11 @@ export default async function Tierlists() {
               className="flex flex-col items-center gap-2 rounded-xl border p-2"
               key={tierlist.id}
             >
-              <span className="text-sm font-bold">{tierlist.user?.name}</span>
+              <span className="text-sm font-bold">{tierlist.name}</span>
               <Link href={`/tierlists/${tierlist.id}`}>
                 <div className="overflow-hidden rounded-md border">
                   <div className="w-24">
-                    <img src={tierlist.user?.image} alt="user" />
+                    <img src={tierlist.image} alt="user" />
                   </div>
                 </div>
               </Link>
